4f667457ac70e737c1fe840584e45fbe8a1d21e2
PieroMeza10 <pieroms0905@gmail.com>
2025-06-19 19:30:08 -0500

    bn2

--- src/btree/BNode.java
+++ src/btree/BNode.java
@@ -8,3 +8,9 @@ public class BNode<E extends Comparable<E>> {
     protected ArrayList<E> keys;
     protected ArrayList<BNode<E>> childs;
     protected int count;
+    
+    public BNode(int n) {
+        this.keys = new ArrayList<>(n);
+        this.childs = new ArrayList<>(n + 1); // n+1 hijos posibles en B-Tree
+        this.count = 0;
+        this.idNode = globalId++; // ID único por nodo
