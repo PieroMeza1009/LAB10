39c9c6fafdd3f26f0bca62bc4736e9b0167535fb
PieroMeza10 <pieroms0905@gmail.com>
2025-06-19 19:44:28 -0500

    bt3

--- src/btree/BTree.java
+++ src/btree/BTree.java
@@ -15,5 +15,22 @@ public class BTree<E extends Comparable<E>> {
     public boolean isEmpty() {
         return this.root == null;
     }
+    // Inserta una nueva clave en el árbol
+    public void insert(E cl) {
+        up = false;
+        E mediana;
+        BNode<E> pnew;
+
+        mediana = push(this.root, cl);
+
+        if (up) {
+            pnew = new BNode<>(this.orden);
+            pnew.count = 1;
+            pnew.keys.set(0, mediana);
+            pnew.childs.set(0, this.root);
+            pnew.childs.set(1, nDes);
+            this.root = pnew;
+        }
+    }
 
 }
