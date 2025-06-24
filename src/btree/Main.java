fc166ed15ebd40b2e1a71af45d8e7465cd3bc2b9
PieroMeza10 <pieroms0905@gmail.com>
2025-06-23 23:26:57 -0500

    main2

--- src/btree/Main.java
+++ src/btree/Main.java
@@ -4,5 +4,7 @@ import btree.BTree;
 import excepciones.ItemNoFound;
 
 public class Main {
-
+    public static void main(String[] args) {
+        // Crear manualmente un BTree con orden 4
+        BTree<Integer> arbol = new BTree<>(4);
 }
