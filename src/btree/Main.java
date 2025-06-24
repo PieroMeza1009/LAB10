767ef0ed67768cd049e808b196440af1ca00b591
PieroMeza10 <pieroms0905@gmail.com>
2025-06-23 23:27:13 -0500

    main3

--- src/btree/Main.java
+++ src/btree/Main.java
@@ -7,4 +7,11 @@ public class Main {
     public static void main(String[] args) {
         // Crear manualmente un BTree con orden 4
         BTree<Integer> arbol = new BTree<>(4);
+
+        System.out.println("=== INSERTANDO CLAVES ===");
+        int[] claves = {25, 10, 16, 30, 40, 1, 3, 8, 12, 15, 18, 19, 21, 27, 28, 33, 36, 39, 42, 45};
+
+        for (int clave : claves) {
+            arbol.insert(clave);
+        }
 }
