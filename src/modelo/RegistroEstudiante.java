a8dc82efa6e6affe1444ee4dfb9eda95fe56739d
PieroMeza10 <pieroms0905@gmail.com>
2025-06-24 00:02:09 -0500

    4ejer4

--- src/modelo/RegistroEstudiante.java
+++ src/modelo/RegistroEstudiante.java
@@ -9,4 +9,8 @@ public class RegistroEstudiante implements Comparable<RegistroEstudiante> {
         this.codigo = codigo;
         this.nombre = nombre;
     }
+    public int getCodigo() { return codigo; }
+
+    public String getNombre() { return nombre; }
+
 }
