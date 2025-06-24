ad3ab0b32ec6c7dba3a0cdba9c3685554e093b3e
PieroMeza10 <pieroms0905@gmail.com>
2025-06-24 00:07:33 -0500

    4ejer8

--- src/modelo/RegistroEstudiante.java
+++ src/modelo/RegistroEstudiante.java
@@ -1,6 +1,5 @@
 package modelo;
 
-public class RegistroEstudiante {
 public class RegistroEstudiante implements Comparable<RegistroEstudiante> {
     private int codigo;
     private String nombre;
